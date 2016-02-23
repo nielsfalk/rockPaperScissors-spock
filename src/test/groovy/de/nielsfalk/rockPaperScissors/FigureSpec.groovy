dbf0e01a09f43526e8b839040b77c01d61529968
Niels Falk <niels@nielsfalk.de>
2016-02-23 23:28:28 +0100

    introduced decision table with unrolling

--- src/test/groovy/de/nielsfalk/rockPaperScissors/FigureSpec.groovy
+++ src/test/groovy/de/nielsfalk/rockPaperScissors/FigureSpec.groovy
@@ -1,6 +1,7 @@
 package de.nielsfalk.rockPaperScissors
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static de.nielsfalk.rockPaperScissors.Figure.rock
 import static de.nielsfalk.rockPaperScissors.Figure.scissors
@@ -8,9 +9,15 @@ import static de.nielsfalk.rockPaperScissors.Figure.scissors
 /**
  * @author Niels Falk
  */
-class FigureSpec extends Specification{
-    def "rock should beat scissors"() {
+class FigureSpec extends Specification {
+
+    @Unroll
+    def "#first beats #second"() {
         expect:
-        rock > scissors
+        first > second
+
+        where:
+        first | second
+        rock  | scissors
     }
 }
